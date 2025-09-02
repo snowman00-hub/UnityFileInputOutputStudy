0afdfb1d0715eb0636749ee432fd278cbfc8a55b
윤준모 <ojm01088021296@gmail.com>
2025-09-02 22:14:02 +0900

    녹화, 재생 완성

---
+++ study/Assets/Scripts/Movement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public static readonly string AxisHorizontal = "Horizontal";
+    public static readonly string AxisVertical = "Vertical";
+
+    public float speed = 5f;
+    
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        float h = Input.GetAxis(AxisHorizontal);
+        float v = Input.GetAxis(AxisVertical);
+
+        Vector3 dir = new Vector3(h, 0, v);
+        rb.linearVelocity = dir.normalized * speed;
+    }
+}
