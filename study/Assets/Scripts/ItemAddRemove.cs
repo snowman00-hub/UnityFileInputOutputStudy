16259b7eeb8061fff04747455c576ea86858bbf8
윤준모 <ojm01088021296@gmail.com>
2025-09-03 18:04:23 +0900

    인벤토리 만드는 중

---
+++ study/Assets/Scripts/ItemAddRemove.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ItemAddRemove : MonoBehaviour
+{
+    public void AddItem()
+    {
+
+    }
+
+    public void RemoveItem()
+    {
+
+    }
+}
--- study/Assets/Scripts/ItemTableTest.cs
+++ study/Assets/Scripts/ItemTableTest.cs
@@ -5,19 +5,21 @@ using UnityEngine.UI;
 public class ItemTableTest : MonoBehaviour
 {
     public string id;
+
     public Image image;
-    public TextMeshProUGUI text;
+    public TextMeshProUGUI ItemType;
+    public TextMeshProUGUI Value;
+    public TextMeshProUGUI Price;
+    public TextMeshProUGUI Describe;
 
     private void Start()
     {
         var itemInfo = DataTableManager.ItemTable.Get(id);
 
         image.sprite = Resources.Load<Sprite>(itemInfo.IconFilePath);
-
-        text.text = $"{itemInfo.IconFilePath}\n" +
-            $"{itemInfo.ItemType}\n" +
-            $"{itemInfo.Value}\n" +
-            $"{itemInfo.Price}\n" +
-            $"{itemInfo.Description}";
+        ItemType.text = itemInfo.ItemType.ToString();
+        Value.text = itemInfo.Value;
+        Price.text = itemInfo.Price;
+        Describe.text = itemInfo.Description;
     }
 }
