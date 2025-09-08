cf83b7c81273231b2637a5179a93863ece6347a9
윤준모 <ojm01088021296@gmail.com>
2025-09-08 14:49:26 +0900

    키보드 세팅

--- study/Assets/UI/Scripts/GameOverWindow.cs
+++ study/Assets/UI/Scripts/GameOverWindow.cs
@@ -9,7 +9,9 @@ public class GameOverWindow : GenericWindow
     public Button nextButton;
     public List<TextMeshProUGUI> statTexts;
     public TextMeshProUGUI totalScore;
-    public float writeTextInterval = 0.2f;    
+    public float writeTextInterval = 0.2f;
+
+    private Coroutine coroutine;
 
     private void Awake()
     {
@@ -18,14 +20,21 @@ public class GameOverWindow : GenericWindow
 
     public override void Open()
     {
-        foreach(var text in statTexts)
+        foreach (var text in statTexts)
         {
             text.text = string.Empty;
         }
         totalScore.text = string.Empty;
         firstSelected = nextButton.gameObject;
+
         base.Open();
-        StartCoroutine(CoWriteStats());
+
+        if(coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+
+        coroutine = StartCoroutine(CoWriteStats());
     }
 
     public void OnClickNextButton()
@@ -36,13 +45,15 @@ public class GameOverWindow : GenericWindow
     public IEnumerator CoWriteStats()
     {
         int i = 1;
-        foreach(var text in statTexts)
+        foreach (var text in statTexts)
         {
             string stat = $"{Random.Range(0, 9999):D4}".PadLeft(10);
             text.text = $"Stat {i++} {stat}";
             yield return new WaitForSeconds(writeTextInterval);
         }
 
-        totalScore.text = $"{Random.Range(0,int.MaxValue):D10}";
+        totalScore.text = $"{Random.Range(0, int.MaxValue):D10}";
+
+        coroutine = null;
     }
 }
